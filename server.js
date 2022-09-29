9dc32f03d84fa346a4ba5caa474cdbed6bbc5c05
Joseph Lara <vipercar233@gmail.comgit config --global user.name Joseph>
2022-09-29 01:33:50 -0500

    created an add employee inquirer function after select 'Add Employee in inquirer case'

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const db = require('./db/connection');
 const inquirer = require('inquirer');
+const Employee = require('../../Module 10/team-profile-generator/lib/Employee');
 
 
 db.connect(err => {
@@ -36,7 +37,7 @@ const startApp = () => {
                 console.log('View All Employees')
                 break;
             case "Add Employee":
-                console.log('Add Employee')
+                addEmployee()
                 break;
             case "Update Employee Role":
                 console.log('Update Employee Role')
@@ -51,5 +52,59 @@ const startApp = () => {
                 console.log('Add Department')
                 break;
         }
+    });
+};
+
+const addEmployee = () => {
+    return inquirer.prompt ([
+        {
+            type: 'input',
+            name: 'first_name',
+            message: "Employee's first name? ",
+            validate: fnInput => {
+                if(fnInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a valid first name!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'last_name',
+            message: "Employee's last name? ",
+            validate: lnInput => {
+                if(lnInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a valid last name!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'role_id',
+            message: "Employee's role? ",
+            validate: roleInput => {
+                if(roleInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a role!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'manager_id',
+            message: "Employee's manager? "
+        },
+
+    ])
+    .then(answers => {
+        console.log(`${answers.first_name} ${answers.last_name} has added to database!`);
+        startApp();
     });
 };
