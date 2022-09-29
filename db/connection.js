c1067a36005c597b63fa90106cce4e31a85b783c
Joseph Lara <vipercar233@gmail.comgit config --global user.name Joseph>
2022-09-29 00:53:41 -0500

    Added first part of inquirer prompt with choices

--- db/connection.js
+++ db/connection.js
@@ -10,9 +10,9 @@ const db = mysql.createConnection(
         host: 'localhost',
         user: db_user,
         password: db_pw,
-        database: 'employee'
+        database: 'employee_db'
     },
-    console.log('Connected to the employee database')
+    console.log('Connected to the employee_db database')
 )
 
 module.exports = db;
--- server.js
+++ server.js
@@ -1,6 +1,55 @@
 const db = require('./db/connection');
+const inquirer = require('inquirer');
+
 
 db.connect(err => {
     if (err) throw err;
-    console.log('Database connected.');
+    console.log(`
+    +=======================================================+
+    | HELLO EMPLOYEE MANAGER                                |
+    |                                                       |
+    +=======================================================+
+    `)
+    startApp();
 });
+
+// prompt inquirer
+const startApp = () => {
+    return inquirer.prompt ([
+        {
+            type: 'list',
+            name: 'option',
+            message: 'What would you like to do?',
+            choices: [
+                'View All Employees',
+                'Add Employee',
+                'Update Employee Role',
+                'View All Roles',
+                'View All Departments',
+                'Add Department'
+            ]
+        }
+    ])
+    .then(answers => {
+        switch (answers.option){
+            case "View All Employees":
+                console.log('View All Employees')
+                break;
+            case "Add Employee":
+                console.log('Add Employee')
+                break;
+            case "Update Employee Role":
+                console.log('Update Employee Role')
+                break;
+            case "View All Roles":
+                console.log('View All Roles')
+                break;
+            case "View All Departments":
+                console.log('View All Departments')
+                break;
+            case "Add Department":
+                console.log('Add Department')
+                break;
+        }
+    });
+};
